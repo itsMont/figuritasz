d165bf9c7eb83f0393fa5134617bd7c80803703a
itsMont <jmontanaa@unal.edu.co>
2022-06-06 01:10:00 -0500

    Aplicación calculando Areas y soportando errores de ingreso

--- src/View/MenuPrincipal.java
+++ src/View/MenuPrincipal.java
@@ -36,24 +36,31 @@ public class MenuPrincipal implements ActionListener {
     // ComboBox para opciones de selección figura
     private JComboBox opciones;
     
-    // Método de actionPerfomed
+     // Método de actionPerfomed para seleccionar
     @Override
-    public void actionPerformed(ActionEvent e){
+    public void actionPerformed(ActionEvent e)
+    {
         anuncio2.setText("Seleccionada Figura " + opciones.getSelectedItem());
         panelPrincipal.add(botonAccion);
         panelPrincipal.add(anuncio1);
         panelPrincipal.add(campoIngreso);
-        if (opciones.getSelectedItem() == "Cilindro") {
+        
+        // Cada vez que seleccione una figura reinicia el label del resultado del área
+        resultadoArea.setText("Área");
+        
+        if (opciones.getSelectedItem() == "Cilindro") 
+        {
             panelPrincipal.add(ingresarAltura);
             panelPrincipal.add(campoAltura);
-        }else{
+        }
+        else
+        {
             // Si despues de seleccionar cilindro selecciona otra figura
             // Elimina los campos de cilindro
             panelPrincipal.remove(ingresarAltura);
             panelPrincipal.remove(campoAltura);
         }
     }
-    
     
     public void init(){
         ventanaMenu = new JFrame("Inicio | Figuritasz");
@@ -62,19 +69,14 @@ public class MenuPrincipal implements ActionListener {
         String [] figuras = {"Cuadrado","Circulo","Cilindro"};
         opciones = new JComboBox(figuras);
         
-        // BotonAccion Calcular valores - acciones
-        botonAccion = new JButton("Calcular");
-        botonAccion.setPreferredSize(new Dimension(150,60));
-        botonAccion.setFont(new Font("Roboto Light", Font.PLAIN, 18));
-        botonAccion.addActionListener(this);
-        
         // Boton Seleccion Figura
         botonSeleccion = new JButton("Seleccionar");
         botonSeleccion.setPreferredSize(new Dimension(150,60));
         botonSeleccion.setFont(new Font("Roboto Light", Font.PLAIN, 18));
         botonSeleccion.addActionListener(this);
         
-        resultadoArea = new JLabel("Area: ");
+        // Deja declarado este Label para que después tenga un valor
+        resultadoArea = new JLabel();
         // Label Anuncio 1
         anuncio1 = new JLabel("Ingrese número");
         anuncio1.setFont(new Font("Roboto Black", Font.PLAIN, 25));
@@ -98,6 +100,50 @@ public class MenuPrincipal implements ActionListener {
         imgLabel = new JLabel(img);
         imgLabel.setPreferredSize(new Dimension(500, 900));
         */
+        
+        // BOTONES
+        
+         // BotonAccion Calcular valores - acciones
+        botonAccion = new JButton("Calcular");
+        botonAccion.setPreferredSize(new Dimension(150,60));
+        botonAccion.setFont(new Font("Roboto Light", Font.PLAIN, 18));
+        
+        // Agregar acción personalizada para este botón mediante clase anonima
+        botonAccion.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                
+                // Saber qué tipo de figura es para realizar operaciones
+                try{
+                    Double num = Double.parseDouble(campoIngreso.getText());
+                    String tipoFigura = String.valueOf(opciones.getSelectedItem());
+                    switch(tipoFigura){
+                        case "Cuadrado":
+                            Cuadrado square = new Cuadrado(num);
+                            resultadoArea.setText("Área: " + String.valueOf(square.getArea()) );
+                            break;
+                        case "Cilindro":
+                            Double altura = Double.parseDouble(campoAltura.getText());
+                            Cilindro cilindro = new Cilindro(num, altura);
+                            resultadoArea.setText("Área: " + String.valueOf(cilindro.getArea()) );
+                            break;
+                        case "Circulo":
+                            Circulo circle = new Circulo(num);
+                            resultadoArea.setText("Área: " + String.valueOf(circle.getArea()) );                
+                            break;
+                    }
+                } 
+                // En caso de que ocurra algo con el ingreso de datos en el formulario (Vacío u otro tipo de dato)
+                catch(NumberFormatException exc){
+                    JOptionPane.showMessageDialog(ventanaMenu,
+                    "No ha ingresado un número en el campo designado.",
+                    "Advertencia",
+                    JOptionPane.WARNING_MESSAGE);
+                }
+                
+                
+            }
+        });
         
         // Valores del panelPrincipal 
         // Border(int top, int left, int bottom, int right)
